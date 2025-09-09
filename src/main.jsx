d8928ea9358a8114b72a821b7fb821d379c9891e
enzokrebs8 <enzokrebs8@gmail.com>
2025-09-09 12:35:17 -0300

    Frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../index.css'
-import Home from '../Home.jsx'
+import Home from './pages/Home/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
